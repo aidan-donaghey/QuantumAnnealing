748375e574d5447368e0ec6c236dca8eda7e03e2
Aidan Donaghey <aidan.donaghey@yale.edu>
2021-08-16 14:31:18 -0400

    Finished First Theming and figures working- sessions updated

--- googlemapsStreamlit.py
+++ googlemapsStreamlit.py
@@ -21,8 +21,16 @@ gmaps = googlemaps.Client(key = os.getenv('GMAPS_API'))
 cities = []
 infoList = []
 ordredcities = []
-ss = SessionState.get(cities = cities, infoList = infoList, ordredcities = ordredcities)
-citiesdata = pd.DataFrame(ss.infoList, columns =["Location","lat","lng"] )
+# ss = SessionState.get(cities = cities, infoList = infoList, ordredcities = ordredcities)
+# Initialization
+if 'cities' not in st.session_state:
+    st.session_state['cities'] = cities
+if 'infoList' not in st.session_state:
+    st.session_state['infoList'] = infoList
+if 'ordredcities' not in st.session_state:
+    st.session_state['ordredcities'] = ordredcities
+
+citiesdata = pd.DataFrame(st.session_state['infoList'], columns =["Location","lat","lng"] )
 
 components.title_Intro()
 
@@ -46,16 +54,16 @@ submit_button = form.form_submit_button(label='Submit')
 
 
 if submit_button:
-  ss.cities.append(gmaps.geocode(text))
-  ss.infoList.append([gmaps.geocode(text)[0]["formatted_address"],gmaps.geocode(text)[0]['geometry']['location']['lat'] ,gmaps.geocode(text)[0]['geometry']['location']['lng']])
-  citiesdata = pd.DataFrame(ss.infoList, columns =["Location","lat","lng"] )
+  st.session_state.cities.append(gmaps.geocode(text))
+  st.session_state.infoList.append([gmaps.geocode(text)[0]["formatted_address"],gmaps.geocode(text)[0]['geometry']['location']['lat'] ,gmaps.geocode(text)[0]['geometry']['location']['lng']])
+  citiesdata = pd.DataFrame(st.session_state.infoList, columns =["Location","lat","lng"] )
   
 
 
 
 
 #st.write(f"You have currently Selected {len(ss.infoList)} cities")
-if len(ss.infoList)!=0:
+if len(st.session_state.infoList)!=0:
   st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
   st.table(citiesdata)
   st.markdown("### Unoptimised Map")
@@ -69,15 +77,15 @@ QAbutton = st.button("Run the QA")
 if QAbutton:
   bestsample = qa.RunQA(qa.setcities(citiesdata))
   sortedOrder = quantumCityOrder(qa.setcities(citiesdata),bestsample)
-  ss.ordredcities = citiesdata.reindex(sortedOrder)
-  df2 = ss.ordredcities.iloc[0]
-  ss.ordredcities = ss.ordredcities.append(df2, ignore_index = True)
-  centerLocation = (ss.ordredcities["lat"].mean(), ss.ordredcities["lng"].mean())
+  st.session_state.ordredcities = citiesdata.reindex(sortedOrder)
+  df2 = st.session_state.ordredcities.iloc[0]
+  st.session_state.ordredcities = st.session_state.ordredcities.append(df2, ignore_index = True)
+  centerLocation = (st.session_state.ordredcities["lat"].mean(), st.session_state.ordredcities["lng"].mean())
 
-if len(ss.ordredcities)!=0:
+if len(st.session_state.ordredcities)!=0:
   st.title("This is the fasteset route to hit all places and return home")
   projection2 = st.radio('Choose a Map Projection', ["equirectangular","orthographic","natural earth",], key="qaradio")
-  components.plotMap(ss.ordredcities, projection2)
+  components.plotMap(st.session_state.ordredcities, projection2)
 
 
 
